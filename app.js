58d7232c656106c6d2ce22120ad63c0fb0ac1dd6
User <priyanshi220383@gmail.com>
2026-08-11 20:31:49 +0530

    Day 3 work

--- app.js
+++ app.js
@@ -28,5 +28,36 @@
 // console.log(process.argv[2]);
 
 // dot env module is used to load environment variables from a .env file into process.env 
- require('dotenv').config();
- console.log(process.env.PORT);
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`
+            <h1 style='color:red'>Hello World</h1>
+            <p>Welcome to my first node js application</p>
+            `);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'aplication/json' });
+        res.end(JSON.stringify({ error: 'Route not found' }));
+
+    }
+});
+server.listen(3000, () => console.log('running on : 3000'));
+// post request try 
+   
+
+// require('dotenv').config();
+// const Port = process.env.PORT|| 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+
+//     res.write('<h1>Hello World</h1>');
+//     res.write('<p>Welcome to my first node js application</p>');
+
+//     res.end();
+// });
+
+// server.listen(Port, () => {
+//     console.log(`Server is running on port ${Port}`);
+// });
